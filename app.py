3558810774411ca1c390f62ae57a228a00f49857
Sandeep Somasekharan <codereverser@gmail.com>
2021-05-10 14:01:47 +0530

    api: update /process API

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 import asyncio
+from typing import Dict
+
+from fastapi import FastAPI, File, Form, UploadFile
+from fastapi.responses import HTMLResponse, JSONResponse
+from pydantic import BaseModel
 
 from casparser import read_cas_pdf
-from fastapi import FastAPI, File, Form, UploadFile
-from fastapi.responses import HTMLResponse
+from casparser.types import CASParserDataType
+
+
+class CASResponse(BaseModel):
+    status: str
+    message: str
+    cas: CASParserDataType
+
+
+class CASErrorResponse(CASResponse):
+    status: str = "error"
+    cas: Dict
 
 
 app = FastAPI()
@@ -13,8 +28,21 @@ def index():
     return """<html></html>"""
 
 
-@app.post("/process")
+@app.post(
+    "/api/process/",
+    response_model=CASResponse,
+    responses={400: {"model": CASErrorResponse}},
+    summary="Process CAS PDF file.",
+)
 async def process_cas(password: str = Form(...), cas: UploadFile = File(...)):
+    """Process CAS PDF file."""
     loop = asyncio.get_running_loop()
-    result = await loop.run_in_executor(None, read_cas_pdf, cas.file, password)
+    result = {"status": "error", "message": "Unknown Error", "cas": {}}
+    try:
+        cas = await loop.run_in_executor(None, read_cas_pdf, cas.file, password)
+        result.update(status="OK", message="", cas=cas)
+    except Exception as e:
+        result.update(message=str(e))
+    if result["status"] == "error":
+        return JSONResponse(result, status_code=400)
     return result

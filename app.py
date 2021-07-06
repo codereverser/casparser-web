b54f58af3c8d96474788e5ea73132ebb1ea3467b
Sandeep Somasekharan <codereverser@gmail.com>
2021-07-06 20:24:42 +0530

    api: support for capital gains

--- app.py
+++ app.py
@@ -1,19 +1,49 @@
 import asyncio
-from typing import Dict
+from datetime import date
+from decimal import Decimal
+import itertools
+from typing import Dict, List, Optional
+from typing_extensions import TypedDict
 
 from fastapi import FastAPI, File, Form, Request, UploadFile
 from fastapi.responses import JSONResponse, PlainTextResponse
 from mangum import Mangum
 from pydantic import BaseModel
 
-from casparser import read_cas_pdf
+from casparser import read_cas_pdf, CapitalGainsReport
+from casparser.analysis.gains import Fund
+from casparser.exceptions import IncompleteCASError
 from casparser.types import CASParserDataType
+
+
+class FundType(TypedDict):
+    name: str
+    isin: str
+    type: str
+
+
+class GainEntryType(TypedDict):
+    fy: str
+    fund: str
+    isin: str
+    type: str
+    buy_date: date
+    buy_price: Decimal
+    stamp_duty: Decimal
+    sell_date: date
+    sell_price: Decimal
+    stt: Decimal
+    units: Decimal
+    ltcg: Decimal
+    stcg: Decimal
+    tax_ltcg: Decimal
 
 
 class CASResponse(BaseModel):
     status: str
     message: str
     cas: CASParserDataType
+    gains: Optional[Dict]
 
 
 class CASErrorResponse(CASResponse):
@@ -31,6 +61,57 @@ def ping():
     return PlainTextResponse("pong")
 
 
+def prepare_gains(cg: Optional[CapitalGainsReport]):
+    if cg is None:
+        return None
+    summary = {}
+    fy: str
+    fund: Fund
+    for (fy, fund), txns in itertools.groupby(cg.gains, key=lambda x: (x.fy, x.fund)):
+        if fy not in summary:
+            summary[fy] = {
+                "funds": [],
+                "total": {
+                    "ltcg": 0,
+                    "stcg": 0,
+                    "tax_ltcg": 0,
+                },
+            }
+        net_total = summary[fy]["total"]
+        total = {
+            "ltcg": 0,
+            "stcg": 0,
+            "tax_ltcg": 0,
+        }
+        data = {"fy": fy, "fund": fund, "txns": []}
+        for txn in txns:
+            total["ltcg"] += txn.ltcg
+            total["stcg"] += txn.stcg
+            total["tax_ltcg"] += txn.ltcg_taxable
+            data["txns"].append(
+                {
+                    "buy_date": txn.buy_date,
+                    "buy_price": txn.buy_price,
+                    "units": txn.units,
+                    "coa": txn.coa,
+                    "sell_date": txn.sell_date,
+                    "sell_price": txn.sell_price,
+                    "ltcg": txn.ltcg,
+                    "stcg": txn.stcg,
+                    "tax_ltcg": txn.ltcg_taxable,
+                }
+            )
+
+        data["total"] = total
+        summary[fy]["funds"].append(data)
+
+        net_total["ltcg"] += total["ltcg"]
+        net_total["stcg"] += total["stcg"]
+        net_total["tax_ltcg"] += total["tax_ltcg"]
+        summary[fy]["total"] = net_total
+    return summary
+
+
 @app.post(
     "/api/process/",
     response_model=CASResponse,
@@ -40,10 +121,14 @@ def ping():
 async def process_cas(password: str = Form(...), cas: UploadFile = File(...)):
     """Process CAS PDF file."""
     loop = asyncio.get_running_loop()
-    result = {"status": "error", "message": "Unknown Error", "cas": {}}
+    result = {"status": "error", "message": "Unknown Error", "cas": {}, "gains": None}
     try:
         cas = await loop.run_in_executor(None, read_cas_pdf, cas.file, password)
-        result.update(status="OK", message="", cas=cas)
+        try:
+            cg_report = CapitalGainsReport(cas)
+        except IncompleteCASError:
+            cg_report = None
+        result.update(status="OK", message="", cas=cas, gains=prepare_gains(cg_report))
     except Exception as e:
         result.update(message=str(e))
     if result["status"] == "error":

29411d48b20b99757d716ab61143eb8b2aadda63
Sandeep Somasekharan <codereverser@gmail.com>
2023-02-18 21:23:11 +0530

    add eslint/prettier support

--- ui/components/casform.vue
+++ ui/components/casform.vue
@@ -5,18 +5,20 @@
             template(#title) CASParser Demo
             template(#content)
                 .flex.flex-row.justify-content-end.file-select
-                    InputText.mr-4.col-8(placeholder="Select a file" :disabled="true" v-model="filename")
+                    InputText.mr-4.col-8(v-model="filename" placeholder="Select a file" :disabled="true")
                     Button.mr-4.file-select(label="Select" @click="selectFile")
-                    input(ref="file" type="file" @change="fileSelected" :disabled="false" accept="application/pdf")
+                    input(ref="file" type="file" :disabled="false" accept="application/pdf" @change="fileSelected" )
             template(#footer)
                 .flex.justify-content-end.align-items-start
                     .flex.flex-column
-                        Password.mr-4(v-model="password" :toggleMask="true"
-                                        :feedback="false" placeholder="Enter CAS Password"
-                                        :class="{'p-invalid': v$.$errors.length > 0}")
+                        Password.mr-4(
+                          v-model="password" :toggle-mask="true"
+                          :feedback="false" placeholder="Enter CAS Password"
+                          :class="{'p-invalid': v$.$errors.length > 0}")
                         small(:class="{'p-invisible': v$.$errors.length === 0}").text-red-400 {{ formErrorText }}
-                    Button.mr-4(label="Submit" @click="submit"
-                                    :disabled="password.length <= 5" :loading="loading")
+                    Button.mr-4(
+label="Submit" :disabled="password.length <= 5"
+                                    :loading="loading" @click="submit")
 ProgressBar(mode="indeterminate" :class="{'invisible': !loading}" style="height: 3px;")
 .text-center.text-red-400.mt-2(:class="{'invisible': serverErrorText.length === 0}") {{ serverErrorText }}
 </template>
@@ -26,97 +28,96 @@ import { useVuelidate } from "@vuelidate/core"
 import type { ValidationRuleWithoutParams } from "@vuelidate/core"
 import { minLength, required } from "@vuelidate/validators"
 
-const emit = defineEmits(['cas-parsed'])
+const emit = defineEmits(["cas-parsed"])
 
-const password = ref("");
-const serverErrorText = ref("");
+const password = ref("")
+const serverErrorText = ref("")
 
-const file = ref<HTMLElement | null>(null);
-const filename = ref("");
-const selectedFile = ref<File | null>(null);
+const file = ref<HTMLElement | null>(null)
+const filename = ref("")
+const selectedFile = ref<File | null>(null)
 const selectFile = () => {
-    if (file.value instanceof HTMLElement) file.value.click();
-};
+  if (file.value instanceof HTMLElement) file.value.click()
+}
 const fileSelected = (ev: Event) => {
-    filename.value = "";
-    const files = (ev.target as HTMLInputElement).files;
-    if (files?.length == 1) {
-        selectedFile.value = files[0];
-        filename.value = files[0].name;
-    }
-};
+  filename.value = ""
+  const files = (ev.target as HTMLInputElement).files
+  if (files?.length === 1) {
+    selectedFile.value = files[0]
+    filename.value = files[0].name
+  }
+}
 
 // Validations
 const fileRequired: ValidationRuleWithoutParams = {
-    $validator: (el): boolean => {
-    return el instanceof File;
-    },
-    $message: "File missing!",
-};
+  $validator: (el): boolean => {
+    return el instanceof File
+  },
+  $message: "File missing!",
+}
 const fileMaxSize: ValidationRuleWithoutParams = {
-    $validator: (el): boolean => {
-    return el instanceof File && el.size <= 1048576;
-    },
-    $message: "File should be less than 1MB.",
-};
+  $validator: (el): boolean => {
+    return el instanceof File && el.size <= 1048576
+  },
+  $message: "File should be less than 1MB.",
+}
 
 const rules = {
-    password: { required, minLength: minLength(5) },
-    selectedFile: { fileMaxSize, fileRequired },
-};
-const v$ = useVuelidate(rules, { selectedFile, password });
+  password: { required, minLength: minLength(5) },
+  selectedFile: { fileMaxSize, fileRequired },
+}
+const v$ = useVuelidate(rules, { selectedFile, password })
 
 const formErrorText = computed(() => {
-    return v$.value.$errors.length > 0 ? v$.value.$errors[0].$message : "";
-});
+  return v$.value.$errors.length > 0 ? v$.value.$errors[0].$message : ""
+})
 
 // Submit data
-const loading = ref(false);
+const loading = ref(false)
 const submit = async () => {
-    v$.value.$touch();
-    if (v$.value.$invalid) return;
-    if (selectedFile.value === null) return;
-    try {
-        loading.value = true;
-        serverErrorText.value = "";
-        const data = new FormData();
-        data.append("cas", selectedFile.value);
-        data.append("password", password.value);
-        const response = await fetch("/api/process/", {
-            method: "POST",
-            body: data,
-        });
-        const casData = await response.json();
-        const { status, message, cas, gains, stats } = casData;
-        if (status !== "error") {
-            emit("cas-parsed", { cas, gains, stats, status, message });
-        } else {
-            serverErrorText.value = message;
-            emit("cas-parsed", {
-            cas: null,
-            gains: null,
-            stats: null,
-            status: status,
-            message: message,
-            });
-        }
-    } catch (error: any) {
-        if (Object.prototype.hasOwnProperty.call(error, "message")) {
-            serverErrorText.value = error.message;
-        } else {
-            serverErrorText.value = "Unknown Error. Please try again!";
-        }
-    } finally {
-        loading.value = false;
+  v$.value.$touch()
+  if (v$.value.$invalid) return
+  if (selectedFile.value === null) return
+  try {
+    loading.value = true
+    serverErrorText.value = ""
+    const data = new FormData()
+    data.append("cas", selectedFile.value)
+    data.append("password", password.value)
+    const response = await fetch("/api/process/", {
+      method: "POST",
+      body: data,
+    })
+    const casData = await response.json()
+    const { status, message, cas, gains, stats } = casData
+    if (status !== "error") {
+      emit("cas-parsed", { cas, gains, stats, status, message })
+    } else {
+      serverErrorText.value = message
+      emit("cas-parsed", {
+        cas: null,
+        gains: null,
+        stats: null,
+        status,
+        message,
+      })
     }
-};
-
+  } catch (error: any) {
+    if (Object.prototype.hasOwnProperty.call(error, "message")) {
+      serverErrorText.value = error.message
+    } else {
+      serverErrorText.value = "Unknown Error. Please try again!"
+    }
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <style lang="scss">
 .file-select {
-    input[type="file"] {
-        display: none;
-    }
+  input[type="file"] {
+    display: none;
+  }
 }
 </style>
--- ui/plugins/primevue.js
+++ ui/plugins/primevue.js
@@ -1,30 +1,30 @@
-import { defineNuxtPlugin } from "#app";
-import PrimeVue from "primevue/config";
-import Button from "primevue/button";
-import Card from "primevue/card";
-import Column from "primevue/column";
-import DataTable from "primevue/datatable";
-import Fieldset from "primevue/fieldset";
-import InputSwitch from "primevue/inputswitch";
-import InputText from "primevue/inputtext";
-import Panel from "primevue/panel";
-import Password from "primevue/password";
-import ProgressBar from "primevue/progressbar";
-import TabPanel from "primevue/tabpanel";
-import TabView from "primevue/tabview";
+import { defineNuxtPlugin } from "#app"
+import PrimeVue from "primevue/config"
+import Button from "primevue/button"
+import Card from "primevue/card"
+import Column from "primevue/column"
+import DataTable from "primevue/datatable"
+import Fieldset from "primevue/fieldset"
+import InputSwitch from "primevue/inputswitch"
+import InputText from "primevue/inputtext"
+import Panel from "primevue/panel"
+import Password from "primevue/password"
+import ProgressBar from "primevue/progressbar"
+import TabPanel from "primevue/tabpanel"
+import TabView from "primevue/tabview"
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, {ripple: true});
-    nuxtApp.vueApp.component('Button', Button);
-    nuxtApp.vueApp.component('Card', Card);
-    nuxtApp.vueApp.component('Column', Column);
-    nuxtApp.vueApp.component('DataTable', DataTable);
-    nuxtApp.vueApp.component('Fieldset', Fieldset);
-    nuxtApp.vueApp.component('InputSwitch', InputSwitch);
-    nuxtApp.vueApp.component('InputText', InputText);
-    nuxtApp.vueApp.component('Panel', Panel);
-    nuxtApp.vueApp.component('Password', Password);
-    nuxtApp.vueApp.component('ProgressBar', ProgressBar);
-    nuxtApp.vueApp.component('TabPanel', TabPanel);
-    nuxtApp.vueApp.component('TabView', TabView);
-});
+  nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+  nuxtApp.vueApp.component("Button", Button)
+  nuxtApp.vueApp.component("Card", Card)
+  nuxtApp.vueApp.component("Column", Column)
+  nuxtApp.vueApp.component("DataTable", DataTable)
+  nuxtApp.vueApp.component("Fieldset", Fieldset)
+  nuxtApp.vueApp.component("InputSwitch", InputSwitch)
+  nuxtApp.vueApp.component("InputText", InputText)
+  nuxtApp.vueApp.component("Panel", Panel)
+  nuxtApp.vueApp.component("Password", Password)
+  nuxtApp.vueApp.component("ProgressBar", ProgressBar)
+  nuxtApp.vueApp.component("TabPanel", TabPanel)
+  nuxtApp.vueApp.component("TabView", TabView)
+})

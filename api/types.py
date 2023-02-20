43523f66ea0b22b0e9cb5a2428ea13b432abf0aa
Sandeep Somasekharan <codereverser@gmail.com>
2023-02-20 17:44:33 +0530

    minor bug fixes

--- api/types.py
+++ api/types.py
@@ -3,37 +3,14 @@ from decimal import Decimal
 from typing import Dict, Optional
 from typing_extensions import TypedDict
 
-from casparser.types import CASParserDataType
+from casparser.types import CASData
 from pydantic import BaseModel
-
-
-class FundType(TypedDict):
-    name: str
-    isin: str
-    type: str
-
-
-class GainEntryType(TypedDict):
-    fy: str
-    fund: str
-    isin: str
-    type: str
-    purchase_date: date
-    purchase_value: Decimal
-    stamp_duty: Decimal
-    sale_date: date
-    sale_value: Decimal
-    stt: Decimal
-    units: Decimal
-    ltcg: Decimal
-    stcg: Decimal
-    tax_ltcg: Decimal
 
 
 class CASResponse(BaseModel):
     status: str
     message: str
-    cas: CASParserDataType
+    cas: CASData
     gains: Optional[Dict]
     stats: Optional[Dict]
 

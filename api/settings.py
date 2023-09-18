ad1df7b4c9669575d7c4bae7044499e7e21ee360
Sandeep Somasekharan <codereverser@gmail.com>
2023-09-18 13:07:48 +0530

    update dependencies (use pydantic v2)

--- api/settings.py
+++ api/settings.py
@@ -1,7 +1,8 @@
 import pathlib
 from typing import List, Union
 
-from pydantic import AnyHttpUrl, BaseSettings, Field, validator
+from pydantic import AnyHttpUrl, Field, validator
+from pydantic_settings import BaseSettings
 
 
 BASE_DIR = pathlib.Path(__file__).absolute().parent.parent
